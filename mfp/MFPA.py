7a1dfe9cb195ef7a8b7e4822bd8e2d05c86eb746
lay-bs <163874875+lay-bs@users.noreply.github.com>
2024-04-22 16:00:52 -0300

    Add files via upload

---
+++ mfp/MFPA.py
@@ -0,0 +1,25 @@
+def time_to_deliver(P, strengths, time): #calc num total de pizzas q podem ser entregues
+    total_pizzas = 0                     #em um determinado tempo e usando as forças dos competidores
+    for strength in strengths:
+        total_pizzas += min(time // strength, P)
+    return total_pizzas
+
+def min_time_to_deliver(N, P, strengths):   #determina o menor tempo possível p a equipe completar
+    left = 0                                #a entrega
+    right = min(strengths) * P  
+    
+    while left < right:
+        mid = (left + right) // 2
+        if time_to_deliver(P, strengths, mid) >= P:
+            right = midx'
+        else:
+            left = mid + 1
+            
+    return left
+
+# Input
+N, P = map(int, input().split())
+strengths = list(map(int, input().split()))
+
+# Output
+print(min_time_to_deliver(N, P, strengths))
